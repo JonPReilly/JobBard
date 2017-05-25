a60f7e9ac2da03755a7acced52eb52c7e1367d48
Jon <jonreilly1994@gmail.com>
2017-05-25 13:16:13 -0400

    Created functionality for auto-filling. Page can now populate hard-coded fields. The idea is that these fields will be pulled from the server in the future, so if you are applying for a job we get your info & the info of the specific form, fill it out, and then auto-track the application for you.

--- Chrome Extension/JobBard/job_filler.js
+++ Chrome Extension/JobBard/job_filler.js
@@ -17,5 +17,48 @@ function ajaxSuccess(response)
 {
     console.log(response);
 }
-var res = ajaxRequest("GET", "http://127.0.0.1:8000/", "");
 
+
+function getPageInformation()
+{
+    var page_info = {
+        "first_name" : {"type" : "input", "identifier" : {"attr": "id", "val" : "inputSuccess2"}},
+        "last_name" : {"type" : "input", "identifier" : {"attr": "id", "val" : "inputSuccess3"}}
+    };
+    return page_info;
+}
+
+function getUserInformation()
+{
+    return {"first_name" : "Jonathan", "last_name" : "Reilly"};
+}
+
+function fillForm(page_info, user_info)
+{
+    Object.keys( page_info ).forEach(function( field ){
+        var field_type = page_info[field]["type"];
+        var identifier_key = page_info[field]["identifier"]["attr"];
+        var identifier_val = page_info[field]["identifier"]["val"];
+        var selector = "form " + field_type + "[" +identifier_key +"= '" + identifier_val + "']";
+        var input = document.querySelector(selector);
+
+
+        if (input && user_info[ field ] &&  input.type !== "hidden" && ( input.value = user_info[ field ] ))
+            input.style.backgroundColor = "hsla(180, 100%, 50%, 0.15)";
+
+
+    });
+}
+function fillUserJobForm()
+{
+    var page_info = getPageInformation();
+    if (page_info == {}) return;
+    var user_info = getUserInformation();
+    if (user_info == {}) return;
+
+    fillForm(page_info, user_info);
+}
+
+
+
+fillUserJobForm();
