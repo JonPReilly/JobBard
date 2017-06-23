234a966e78244622066895db3920209d118e582c
Jon <jonreilly1994@gmail.com>
2017-06-23 13:10:48 -0400

    Created the JobSerializer to return job objects in JSON

--- JobBard/job_board/api_serializers.py
+++ JobBard/job_board/api_serializers.py
@@ -7,5 +7,46 @@ class CompanySerializer(serializers.ModelSerializer):
         model = Company
         fields = ['id','name']
 
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
+
+class JobSerializer(serializers.ModelSerializer):
 
 
+    location = serializers.SerializerMethodField()
+    company = serializers.SerializerMethodField()
+    class Meta:
+        model = Job
+        fields = ('id','title','location','company','url','date_created','description')
+
+    def get_company(self,obj):
+        return {
+            'name' : obj.company.name,
+            'id' : obj.company.id
+        }
+    def get_location(self,obj):
+        return {
+                    'location':
+                        {
+                            'id' : obj.city.id,
+                            'city' : obj.city.name,
+                            'region' : obj.city.region.name
+                        }
+                }
+
+class JobApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobApplication
+        fields = "__all__"
+
+class UserSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSettings
+        fields = "__all__"
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = "__all__"
--- JobBard/job_board/api_views.py
+++ JobBard/job_board/api_views.py
@@ -3,17 +3,23 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
-from .models import Company
-from .api_serializers import CompanySerializer
+from .models import *
+from .api_serializers import *
 
 @csrf_exempt
 def companies_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
+
     querey = request.GET.get('q')
     print(querey)
     companies = Company.objects.all()
     serializer = CompanySerializer(companies, many=True)
     return JsonResponse(serializer.data, safe=False)
 
+@csrf_exempt
+def jobs_list(request):
+
+    querey = request.GET.get('q')
+    print(querey)
+    jobs = Job.objects.all().order_by('-date_created')[:1]
+    serializer = JobSerializer(jobs, many=True)
+    return JsonResponse(serializer.data, safe=False)
